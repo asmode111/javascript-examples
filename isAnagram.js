218e7145637b4dba5f9a39b69fb53fc4f6fc41c4
Onur Degerli <onur.degerli@viviotex.com>
2025-04-01 19:20:27 +0300

    Implementing if 2 letters are anagram

---
+++ isAnagram.js
@@ -0,0 +1,32 @@
+const isAnagram  = (str1, str2) => {
+  if (str1 == str2) {
+    return true;
+  }
+
+  if (str1.length != str2.length) {
+    return false;
+  }
+
+
+  for (let i = 0; i < str1.length; i++) {
+    let hasLetter = false;
+    for (let j = 0; j < str1.length; j++) {
+      if (str1[i] == str2[j]) {
+        hasLetter = true
+        break;
+      }
+    }
+
+    if (!hasLetter) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(isAnagram("listen", "silent"));
+console.log(isAnagram("hello", "oehll"));
+console.log(isAnagram("listen", "salent"));
+console.log(isAnagram("listen", "silentt"));
+console.log(isAnagram("listen", "silen"));
