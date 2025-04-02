359783ed6a28fa17a51bf1df0622dc2cc084e214
Onur Degerli <onur.degerli@viviotex.com>
2025-04-02 22:13:43 +0200

    Updating urlify method

--- urlify.js
+++ urlify.js
@@ -1,7 +1,7 @@
-const urlify = (str) => {
-  return str.trim().split(" ").join("%20");
+const urlify = (str, trueLength) => {
+  const actual = str.slice(0, trueLength);
+  return actual.split(" ").join("%20");
 }
 
-console.log(urlify("Mr John Smith"));
-console.log(urlify(" Mr John Smith "));
-console.log(urlify("   Mr John Smith   "));
+console.log(urlify("Mr John Smith", 13));
+console.log(urlify("Mr John Smith    ", 13));
