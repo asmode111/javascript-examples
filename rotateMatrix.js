56b97aedefafb16fd32c2256a5d4534d64ed820f
Onur Degerli <onur.degerli@viviotex.com>
2025-04-03 10:14:32 +0200

    Implementing rotate matrix method

---
+++ rotateMatrix.js
@@ -0,0 +1,36 @@
+const rotateMatrix = (matrix) => {
+
+  const n = matrix.length;
+
+  for (let layer = 0; layer < Math.floor(n / 2); layer++) {
+    const first = layer;
+    const last = n - 1 - layer;
+
+    for (let i = first; i < last; i++) {
+      const offset = i - first;
+      
+      const top = matrix[first][i];
+
+      // left -> top
+      matrix[first][i] = matrix[last - offset][first];
+
+      // bottom -> left
+      matrix[last - offset][first] = matrix[last][last - offset];
+
+      // right -> bottom
+      matrix[last][last - offset] = matrix[i][last];
+
+      // top -> right
+      matrix[i][last] = top;
+    }
+  }
+
+  return matrix;
+}
+
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+console.log(rotateMatrix(matrix));
